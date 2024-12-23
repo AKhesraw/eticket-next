13737e1624757e8a502dd68378755ba529ac2dcd
khesraw <ahmad.khesraw@mail.gsp-af.com>
2024-12-23 15:55:40 +0430

    changed styling and replaced link with button for switching tabs

--- src/components/utils/cta.jsx
+++ src/components/utils/cta.jsx
@@ -1,6 +1,9 @@
 import BookFlightForm from "@/components/forms/book-flight";
+import BookHotelForm from "@/components/forms/book-hotel";
 import { usePathname } from "next/navigation";
+import {useState} from "react";
 export default function CTA() {
+  const [tabIndex, setIndex] = useState(0);
   const location = usePathname();
   const tabs = [
     { id: "/admin", label: "Flight" },
@@ -10,24 +13,24 @@ export default function CTA() {
     { id: "/packages", label: "Packages" }
   ];
   return (
-      <div className="bg-white dark:bg-black mx-auto max-w-7xl overflow-hidden border rounded-2xl">
-          <div className="rounded-b-[30px]  dark:bg-black max-w-7xl mx-auto overflow-hidden">
+      <div className="bg-white dark:bg-black mx-auto max-w-7xl overflow-hidden rounded-b-[30px] border rounded-2xl">
+          <div className="dark:bg-black max-w-7xl mx-auto overflow-hidden">
             <div className="flex flex-col sm:flex-row rounded-t-2xl overflow-hidden">
-              {tabs.map((tab) => (
+              {tabs.map((tab, index) => (
                 <div key={tab.id} className="w-full">
-                    <button key={tab.id}
-                      className={`search-tab-item ${location === tab.id && "search-tab-active"}`}>
+                    <button key={tab.id} onClick={() => setIndex(index)}
+                      className={`search-tab-item ${tabIndex === index && "search-tab-active"}`}>
                       {tab.label}
                     </button>
                 </div>
               ))}
             </div>
             <div>
-              {location === "/admin" && <BookFlightForm />}
-              {location === "/hotels" && <></>}
-              {location === "/cars" && <></>}
-              {location === "/visa" && <></>}
-              {location === "/packages" && <></>}
+              {tabIndex === 0 && <BookFlightForm />}
+              {tabIndex === 1 && <BookHotelForm />}
+              {tabIndex === 2 && <></>}
+              {tabIndex === 3 && <></>}
+              {tabIndex === 4 && <></>}
             </div>
           </div>
     </div>
